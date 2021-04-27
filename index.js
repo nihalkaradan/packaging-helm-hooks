3a8fc42f3908cc3fd9246b38992e7da3229bc62b
Nihal Karadan <nihalkaradan@gmail.com>
2021-04-27 20:06:18 +0530

    Dockerfile created

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`listening at PORT: ${PORT}`);
+  });
