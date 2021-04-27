87efd416710b70bdc56f6ba07c27b92163f52e5b
Nihal Karadan <nihalkaradan@gmail.com>
2021-04-27 20:24:22 +0530

    Docker file modified

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,5 @@ RUN npm install
 
 COPY . .
 
+EXPOSE 8080
 CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+
 app.listen(PORT, () => {
     console.log(`listening at PORT: ${PORT}`);
   });
+
