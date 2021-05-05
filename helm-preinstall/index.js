684fc20cd8c1e33cdcd052c6c2b2db58fd7abf35
Nihal Karadan <nihalkaradan@gmail.com>
2021-05-05 16:57:20 +0530

    pre install code changed

--- helm-preinstall/index.js
+++ helm-preinstall/index.js
@@ -4,9 +4,16 @@ var con = mysql.createConnection({
   host: "mysql",
   user: "root",
   password: "password",
+  database: "mydb"
 });
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    console.log(`${process.env.name}`)
+    var sql = `INSERT INTO shop (name, active) VALUES ("${process.env.name}", true)`;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
